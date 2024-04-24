9780d8ea33d60cc8ec17c27cb50c1044b5a8195a
Afonso Miqueias <afonsogroove@gmail.com>
2024-04-24 08:57:36 +0100

    movendo codigo

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return HttpResponse('HOME 2')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
